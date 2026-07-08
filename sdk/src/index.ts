5607936eebf1cc9ca678d1665e8c6f7e65458a0b
Qoder-Undefined <kaytech37@gmail.com>
2026-07-08 10:21:21 +0100

    index

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -2,3 +2,6 @@ export { OnboardingBridgeSDK } from './bridge';
 export { OffRampIntegration } from './offramp';
 export { assertAccountAddress, assertContractAddress } from './validate';
 export * from './types';
+
+
+
