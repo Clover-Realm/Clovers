4692c7dd9aac77abef3fccd06ed2ad0a96d9f196
Qoder-Undefined <kaytech37@gmail.com>
2026-07-14 11:33:46 +0100

    page

--- Clover-Web/app/page.tsx
+++ Clover-Web/app/page.tsx
@@ -1,17 +1,19 @@
 import { Header } from '@/components/header'
 import { Hero } from '@/components/hero'
+import { HowItWorks } from '@/components/how-it-works'
+import { Stats } from '@/components/stats'
 import { Features } from '@/components/features'
 import { UseCases } from '@/components/use-cases'
 import { CtaSection } from '@/components/cta-section'
 import { Footer } from '@/components/footer'
 
 export default function Page() {
-
-  
   return (
     <main className="w-full bg-background">
       <Header />
       <Hero />
+      <HowItWorks />
+      <Stats />
       <Features />
       <UseCases />
       <CtaSection />
